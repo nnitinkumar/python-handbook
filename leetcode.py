9ae524084a7834e320eee21c4bc18744bbe8671f
nnitinkumar <nitindexkumar@gmail.com>
2022-01-16 18:08:38 +0530

    update

---
+++ leetcode.py
@@ -0,0 +1,15 @@
+##Q : Two sum
+
+#nums is the given list 
+#nums = [2,7,11,15]
+
+#target is the added up result which we require
+#target = 9
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return [i,j]

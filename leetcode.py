7f0e89f533c2dc898ee3b026dc6421649be6261a
nnitinkumar <nitindexkumar@gmail.com>
2022-01-25 23:42:43 +0530

    update

--- leetcode.py
+++ leetcode.py
@@ -42,3 +42,15 @@ class Solution:
         return new_word
         
         
+ -----------------------------------------------------------------        
+ 
+##2114 : Maximum number of words found in sentences
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        m = 0
+        for i in sentences:
+            m = max(m, i.count(" ") + 1)
+        return m 
+        
+        

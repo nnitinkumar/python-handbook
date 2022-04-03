8418af53c216dceb9cb1b8aaf51b38ee7f207de3
nnitinkumar <nitindexkumar@gmail.com>
2022-04-03 06:52:23 +0530

    update

--- leetcode.py
+++ leetcode.py
@@ -82,4 +82,55 @@ if checked:
 else:
     print("No")
     
-    
+-----------------------------------------------------------------
+
+#13: Ronam to numeral
+
+IV is 5
+IX is 9
+XL is 40
+XC is 90
+CD is 400
+CM is 900
+
+E.G.
+LVIII
+L + V + I + I + I
+50 + 5 + 1 + 1 + 1
+
+MCMXCIV
+M + C + M + X + C + I + V
+
+1000 + 100 + 1000 + 10 + 100 + 1 + 5
+2306
+
+
+M + CM + XC + IV
+1000 + 900 + 90 + 4
+1994
+
+--Psuedo code
+case when 
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+
+if I is before V then V - 1 i.e. 5-1 = 4
+if I is before X then X - 1 i.e. 10-1 = 9
+ 
+if X is before L then L - 10 i.e. 50-10 = 40
+if X is before C then C - 10 i.e. 100-10 = 90
+
+if C is before D then D - 1 i.e. 500-100 = 400
+if X is before M then M - 1 i.e. 1000-100 = 900
+
+
+
+
+
+
+

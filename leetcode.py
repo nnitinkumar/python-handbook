a6a22e341ab2eed967e750ad0ce68f835e1b30ec
nnitinkumar <nitindexkumar@gmail.com>
2022-04-07 10:55:29 +0530

    update

--- leetcode.py
+++ leetcode.py
@@ -129,8 +129,30 @@ if C is before D then D - 1 i.e. 500-100 = 400
 if X is before M then M - 1 i.e. 1000-100 = 900
 
 
-
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        lookup = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+        }
+    N = len(s)
+    i = N-1
+    
+    output = 0
+    
+    while i >= 0:
+        if i < N-1 and lookup[s[i]] < lookup[s[i+1]]:
+            output -= lookup[s[i]]
+        else:
+            output += lookup[s[i]]
+        i -= 1
+    return output
+    
 
 
 

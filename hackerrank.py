dd6b413dab113213e600a5c1d047184a3518ff38
nnitinkumar <nitindexkumar@gmail.com>
2022-05-10 10:36:59 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -45,6 +45,8 @@ of any student(s) having the second lowest grade
 
 #####Q Find the difference between 2 and 3 versions
 
+
+#####Python 2.7
 d = {} #Step1-  empty dictionary
 
 for _ in range(int(raw_input())): #Step2 - Range for number of students
@@ -68,7 +70,51 @@ for name in second_lowest: #Step13 - Going through the name each students who go
 
    print name #Step 14 - Printing each name of studnets in separate line
 
+
+#####Python 3
+
+#Number of students
+n = int(input())
+
+#Students and grade lists
+nameList = []
+gradeList = []
+
+#Importing values
+if __name__ == '__main__':
+    for _ in range(n):
+        name = input()
+        score = float(input())
+        nameList.append((score,name))
+        gradeList.append(score)
         
+#Define final lists
+finalNameList = nameList[:]
+finalGradeList = []
+finalList = []
+
+#Removing unwanted values
+for i in range(n):
+    if nameList[i][0] == min(gradeList):
+        finalNameList.remove(nameList[i])
+        
+#Final grade list creation
+for i in range(len(finalNameList)):
+    finalGradeList.append(finalNameList[i][0])
+    
+#Adding wanted values
+for i in range(len(finalNameList)):
+    if finalNameList[i][0] == min(finalGradeList):
+        finalList.append(finalNameList[i][1])
+
+#Sorting the list
+finalList.sort()
+
+#Print result
+for i in range(len(finalList)):
+    print(finalList[i])
+
+
         
  ------------------------------------------------------------------------------
  '''

8d1dfc6cd225503f4e54a7b6e57ace26811eed60
nnitinkumar <nitindexkumar@gmail.com>
2022-06-04 18:11:34 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -223,4 +223,4 @@ def print_full_name(first, last):
 if __name__ == '__main__':
     first_name = input()
     last_name = input()
-    print_full_name(first_name, last_name)
+    print_full_name(first_name, last_name)  

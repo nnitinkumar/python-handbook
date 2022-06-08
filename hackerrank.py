1b05ed05db6cbd16211d6e9ea01e00310fc24aed
nnitinkumar <nitindexkumar@gmail.com>
2022-06-08 08:52:36 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -292,3 +292,10 @@ Then compute and print the result of hash(t)
 '''
 
 
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+
+    t = tuple(integer_list)
+    print(hash(t))
+

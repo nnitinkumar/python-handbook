9ce369516367e717e6ae943b4b8db97142a99cf7
nnitinkumar <nitindexkumar@gmail.com>
2022-06-15 23:40:03 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -391,3 +391,16 @@ if __name__ = '__main__':
  Read a given string, change the character at a given
  index and then print the modified string
  '''
+ 
+ def mutate_string(string, position, character):
+    return
+ 
+ if __name__== '__main__':
+    s = input()
+    i,c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+    
+    
+ 
+ 

7ebd02116d1cbcf87df4091f3cecfa6ce25ebc09
nnitinkumar <nitindexkumar@gmail.com>
2022-06-27 23:50:49 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -471,3 +471,29 @@ if __name__ == "__main__":
  
  '''
  
+ 
+ #!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+
+def solve(s): 
+    l = [word.capitalize() for word in s.split(' ')] 
+    s = ' '.join(l)  
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()

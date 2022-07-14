fd594907803e52b398d1041486e39ffcd219ca0a
nnitinkumar <nitindexkumar@gmail.com>
2022-07-14 11:04:16 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -636,3 +636,28 @@ ljust, center.
 
 '''
 
+#Replace all ____ with rjust, ljust, center
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).______(thickness-1)+c+(c*i).______(thickness-1))
+    
+#Top pillars
+for i in range(thickness+1):
+    print((c*thickness).______(thickness*2)+(c*thickness).______(thickness*6))
+    
+#Middle belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).______(thickness*6))
+
+#Bottom Pillars
+for i in range(thickness+1)):
+    print((c*thickness).______(thickness*2)+(c*thickness).______(thickness*6))
+    
+#Bottom cone
+for i in range(thickness):
+    print(((c*thickness-i-1)).______(thickness)+c+(c*(thickness-i-1)).______(thickness)).______(thickness*6))
+    

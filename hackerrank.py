82e00b89ed43aeaf81286d987a8a79fa4a0aa090
nnitinkumar <nitindexkumar@gmail.com>
2022-07-18 22:12:59 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -680,3 +680,16 @@ IJK
 
 '''
 
+## using recursion
+
+import textwrap
+
+def wrap(string, max_width):
+    return
+    
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+    
+    

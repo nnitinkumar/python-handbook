9bb27bbfb6f9209ca6a64162891278ecf906eabb
nnitinkumar <nitindexkumar@gmail.com>
2022-07-30 12:51:16 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -794,7 +794,14 @@ if __name__ == '__main__':
     fptr.close()
     
     
-    
+## itertools.product()
+
+'''
+This tool computes the cartesian product of input iterables.
+It is equivalent to nested for-loops.
+For example, product (A,B) returns the same as ((x,y) for x in A for y in B)
+
+'''
     
     
     

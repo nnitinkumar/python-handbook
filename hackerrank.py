0cf565e774bddbd31c119ee0322b0a4969090b59
nnitinkumar <nitindexkumar@gmail.com>
2022-08-02 23:34:51 +0530

    update

--- hackerrank.py
+++ hackerrank.py
@@ -816,4 +816,11 @@ print(*product(a, b))
 '''
 A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
 
+
+from collections import Counter
+
+myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
+print Counter(myList)
+Counter({2: 4, 3:4, 1:3, 4:2, 5:1})
+
 '''
